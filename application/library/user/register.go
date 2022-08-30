602464bdfd0aba4c6646778097a70671a571a1b6
Hank Shen <swh@admpub.com>
2022-08-31 00:03:13 +0800

    update

--- application/library/user/register.go
+++ application/library/user/register.go
@@ -1,11 +1,16 @@
 package user
 
 import (
+	"net/url"
+
 	cw "github.com/coscms/webauthn"
 	"github.com/coscms/webauthn/static"
 	"github.com/duo-labs/webauthn/webauthn"
 	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/handler/embed"
+
+	"github.com/admpub/nging/v4/application/cmd/bootconfig"
+	"github.com/admpub/nging/v4/application/library/common"
 )
 
 var global = cw.New(handle)
@@ -20,4 +25,15 @@ func Register(r echo.RouteRegister) {
 	fs.Register(static.JS)
 	g := r.Group(`/webauthn`)
 	g.Get(`/static/*`, embed.File(fs))
+
+	// init webauthn
+	backendURL := common.BackendURL(nil)
+	icon := backendURL + `/public/assets/backend/images/logo.png`
+	u, _ := url.Parse(backendURL)
+	Init(&webauthn.Config{
+		RPDisplayName: bootconfig.SoftwareName, // Display Name for your site
+		RPID:          u.Host,                  // Generally the domain name for your site
+		RPOrigin:      backendURL,              // The origin URL for WebAuthn requests
+		RPIcon:        icon,                    // Optional icon URL for your site
+	})
 }
