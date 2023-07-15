b3f524f13bab4abbcbd9982c8965cd80b2996f7b
Hank Shen <swh@admpub.com>
2023-07-15 15:37:03 +0800

    fix

--- application/library/_customer/webauthn.go
+++ application/library/_customer/webauthn.go
@@ -61,7 +61,7 @@ func (u *CustomerHandle) GetUser(ctx echo.Context, username string, opType cw.Ty
 		return nil, err
 	}
 	u2fList := u2f.Objects()
-	if len(u2fList) == 0 {
+	if opType == cw.TypeLogin && len(u2fList) == 0 {
 		err = ctx.NewError(code.Unsupported, `该用户不支持免密登录`)
 		return nil, err
 	}
